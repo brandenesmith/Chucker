468361bc830275638e252804fb2afbf2f54d1763
Branden Smith <smith.brandene@gmail.com>
2021-03-30 14:53:29 -0500

    Make request and response headers buttons.

--- Sources/Chucker/NetworkItemDetailViewController.swift
+++ Sources/Chucker/NetworkItemDetailViewController.swift
@@ -12,6 +12,20 @@ final class NetworkItemDetailViewController: UIViewController {
     internal var networkListItem: NetworkListItem!
     @IBOutlet weak var requestTextView: UITextView!
     @IBOutlet weak var responseTextView: UITextView!
+    @IBOutlet weak var requestButton: UIButton!
+    @IBOutlet weak var responseButton: UIButton!
+
+    private var requestIsOpen: Bool = true {
+        didSet {
+            updateViewForStateChange()
+        }
+    }
+
+    private var responseIsOpen: Bool = true {
+        didSet {
+            updateViewForStateChange()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,5 +48,31 @@ final class NetworkItemDetailViewController: UIViewController {
         }
         
         responseTextView.attributedText = URLSession.convertResponseToString(response.data, response.response, response.error)
+    }
+
+    private func updateViewForStateChange() {
+        UIView.animate(withDuration: 0.25, animations: {
+            // CATransform3D.MakeRotation((float)(Math.PI / 180 * 45), 0.0001f, 0.0001f, 1);
+            self.requestButton.imageView?.layer.transform = CATransform3DMakeRotation(
+                (self.requestIsOpen) ? CGFloat(Double.pi / 2.0) : CGFloat(-(Double.pi / 2.0)),
+                0,
+                0,
+                0
+            )
+            self.responseButton.imageView?.layer.transform = CATransform3DMakeRotation(
+                (self.requestIsOpen) ? CGFloat(Double.pi / 2.0) : CGFloat(-(Double.pi / 2.0)),
+                0,
+                0,
+                0
+            )
+        })
+    }
+
+    @IBAction private func requestButtonTouched(_ sender: UIButton) {
+        requestIsOpen.toggle()
+    }
+
+    @IBAction private func responseButtonTouched(_ sender: UIButton) {
+        responseIsOpen.toggle()
     }
 }
